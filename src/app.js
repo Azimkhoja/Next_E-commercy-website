8b48cf6bf69c20fa91de48c368804148bc87cc87
Azimkhoja <ahmadkhonovazimkhoja@.com>
2023-02-14 03:18:19 +0500

    Next.store.uz E- commercy website is ready to deploy

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            mahsulot: products,
+            brands:brands,
+            markets:markets
+        }
+    },
+    methods: {
+
+    },
+    mounted(){
+        
+    }
+})
+
+app.mount('#root');
---
+++ src/source.js
@@ -0,0 +1,215 @@
+let products = [
+    {
+        image: "./images/laptop.png",
+        price:"299 000 so’m",
+        title: `Apple Airpods Pro 
+        simsiz quloqchin , Oq rangda   `,
+        ordered: "1230 ta buyurtma",
+
+    },
+    {
+        image: "./images/laptop.png",
+        price:"16 114 285 so'm",
+        title: `Oynali Fotoaparat
+        Canon EOS 6D Body`,
+        ordered: "230 ta buyurtma",
+
+    },
+   
+    {
+        image: "./images/laptop.png",
+        price:"16 114 285 so'm",
+        title: `Oynali Fotoaparat
+        Canon EOS 6D Body`,
+        ordered: "230 ta buyurtma",
+
+    },
+    {
+        image: "./images/laptop.png",
+        price:"4 234 000 so’m",
+        title: "Robot - tozalagich Xiaomi Mi Robot",
+        ordered: "543 ta buyurtma",
+
+    },
+    {
+        image: "./images/laptop.png",
+        price:"1 340 000 so’m",
+        title: `Apple watch 4 `,
+        ordered: "543 ta buyurtma",
+
+    },
+    {
+        image: "./images/laptop.png",
+        price:"1 345 000 so’m",
+        title: `Apple Macbook Pro 
+        13" 2.4 2019 Intel core i5 
+        DDR3 8 GB SSD 256 GB  `,
+        ordered: "565 ta buyurtma",
+
+    },
+    {
+        image: "./images/laptop.png",
+        price:"4 234 000 so’m",
+        title: `Apple Airpods Pro 
+        simsiz quloqchin , Oq rangda   `,
+        ordered: "1230 ta buyurtma",
+
+    },
+    {
+        image: "./images/laptop.png",
+        price:"299 000 so’m",
+        title: `Robot - tozalagich Xiaomi 
+        Mi Robot`,
+        ordered: "230 ta buyurtma",
+
+    },
+    {
+        image: "./images/laptop.png",
+        price:"299 000 so’m",
+        title: `Robot - tozalagich Xiaomi 
+        Mi Robot`,
+        ordered: "543 ta buyurtma",
+
+    },
+    {
+        image: "./images/laptop.png",
+        price:"1 340 000 so’m",
+        title: "Apple watch 4 ",
+        ordered: "876 ta buyurtma",
+
+    },
+    {
+        image: "./images/laptop.png",
+        price:"1 345 000 so’m",
+        title: "Apple Airpods Pro simsiz quloqchin , Oq rangda   ",
+        ordered: "245 ta buyurtma",
+
+    },
+    {
+        image: "./images/laptop.png",
+        price:"299 000 so’m",
+        title: `Apple Macbook Pro 
+        13" 2.4 2019 Intel core i5 
+        DDR3 8 GB SSD 256 GB  `,
+        ordered: "565 ta buyurtma",
+
+    },
+]
+
+let brands = [
+    {
+        name: "hp",
+        img: "./images/hp.svg"
+    },
+    {
+        name: "acer",
+        img: "./images/acer.svg"
+    },
+    {
+        name: "asus brand",
+        img: "./images/asus.svg"
+    },
+    {
+        name: "samsung brand",
+        img: "./images/samsung.svg"
+    },
+    {
+        name: "apple brand",
+        img: "./images/apple.svg"
+    },
+    {
+        name: "msi brand",
+        img: "./images/msi.svg"
+    },
+    {
+        name: "redmi brand",
+        img: "./images/redmi.svg"
+    },
+    {
+        name: "vivo",
+        img: "./images/vivo.svg"
+    },
+    {
+        name: "samsung brand",
+        img: "./images/samsung.svg"
+    },
+    {
+        name: "acer",
+        img: "./images/acer.svg"
+    },
+    {
+        name: "hp",
+        img: "./images/hp.svg"
+    },
+    {
+        name: "asus brand",
+        img: "./images/asus.svg"
+    },
+    {
+        name: "redmi brand",
+        img: "./images/apple.svg"
+    },
+    {
+        name: "vivo",
+        img: "./images/vivo.svg"
+    },
+]
+
+let markets = [
+    {
+        img:"./images/elmakon.svg",
+        name: "elmakon",
+    },
+    {
+        img:"./images/goodzone.svg",
+        name: "goodzone market",
+    },
+    {
+        img:"./images/mediapark.svg",
+        name: "mediapark market",
+    },
+    {
+        img:"./images/texnomart.svg",
+        name: "texnomart market",
+    },
+    {
+        img:"./images/image (11).svg",
+        name: "sandiq",
+    },
+    {
+        img:"./images/elmakon.svg",
+        name: "elmakon",
+    },
+    {
+        img:"./images/mediapark.svg",
+        name: "mediapark market",
+    },
+    {
+        img:"./images/texnomart.svg",
+        name: "texnomart market",
+    },
+    {
+        img:"./images/image (11).svg",
+        name: "sandiq",
+    },
+    {
+        img:"./images/elmakon.svg",
+        name: "elmakon",
+    },
+    {
+        img:"./images/goodzone.svg",
+        name: "goodzone market",
+    },
+    {
+        img:"./images/mediapark.svg",
+        name: "mediapark market",
+    },
+    {
+        img:"./images/texnomart.svg",
+        name: "texnomart market",
+    },
+    {
+        img:"./images/image (11).svg",
+        name: "sandiq",
+    },
+]
